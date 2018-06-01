f04e78ac83e24f6e7668919ce5d0a7c7cddd46e1
Jake Goldman <jgoldman@stuy.edu>
2018-06-01 15:37:39 -0400

    started.

---
+++ mesh.py
@@ -0,0 +1,17 @@
+
+'''
+    mesh_file: the full contents of the mesh_file, should be passed from the parser
+
+    returns: a list of verteces.
+'''
+def v_pass( mesh_file ):
+    pass
+
+
+
+
+'''
+
+'''
+def f_pass( mesh_file ):
+    pass
